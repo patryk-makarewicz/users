73375fbe5b80c313bf3d83c71129ce94188d4239
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2023-03-07 23:36:24 +0100

    Get data in Redux Devtools

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,28 @@
 import { useTranslation } from 'react-i18next';
 import { getUsersList } from '@api/Users/Users.api';
+import { useSelector } from 'react-redux';
+import { RootState, store } from 'src/state/store';
+import { useEffect } from 'react';
+import { fetchUsersList } from 'src/state/users';
 
 const Home = ({ data }: any) => {
   const { t } = useTranslation();
 
-  console.log(data);
+  const UsersList = useSelector((state: RootState) => state.usersList);
+  const statusUsersList = useSelector((state: RootState) => state.usersList.status);
+
+  useEffect(() => {
+    store.dispatch(fetchUsersList());
+  }, []);
+
   return <main>{t('navigation.home')}</main>;
 };
 
-export async function getServerSideProps() {
-  const response = await getUsersList();
-  const data = await response;
+// export async function getServerSideProps() {
+//   const response = await getUsersList();
+//   const data = await response;
 
-  return { props: { data } };
-}
+//   return { props: { data } };
+// }
 
 export default Home;
