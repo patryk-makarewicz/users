3ea13926dc81c6e182e23a3138e10a84f5475f98
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2023-03-07 23:17:22 +0100

    Add to env API_KEY & DB_ID

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,8 +1,7 @@
 export const useAPImocks = false;
 
-const { REACT_APP_DB_ID, REACT_APP_API_KEY } = process.env;
-export const BASE_URL = `https://api.airtable.com/v0/${REACT_APP_DB_ID}`;
+export const BASE_URL = `https://api.airtable.com/v0/${process.env.DB_ID}`;
 
 export const headers = {
-  Authorization: `Bearer ${REACT_APP_API_KEY}`
+  Authorization: `Bearer ${process.env.API_KEY}`
 };
