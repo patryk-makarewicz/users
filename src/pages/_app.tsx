2bca9e3c8ed28decb67333f21a4a17b84f3c61aa
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2023-03-07 23:04:21 +0100

    Add react-redux Provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,17 @@
 import { appWithTranslation } from 'next-i18next';
 import type { AppProps } from 'next/app';
 import '../../i18n';
-
+import { Provider } from 'react-redux';
 import MainLayout from '../layouts/main';
+import { store } from 'src/state/store';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Provider store={store}>
       <MainLayout>
         <Component {...pageProps} />
       </MainLayout>
-    </>
+    </Provider>
   );
 }
 
