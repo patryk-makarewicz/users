7c14771716749b74143b14dfc17d47f5ce34dee6
Patryk Makarewicz <64835775+patryk-makarewicz@users.noreply.github.com>
2023-03-07 23:37:31 +0100

    Get data to Redux Devtools (#2)
    * Turn on strict mode
    
    * Add type to Layout
    
    * Create store
    
    * Create users state
    
    * Add react-redux
    
    * Add react-redux Provider
    
    * Add to env API_KEY & DB_ID
    
    * Create UsersListModel
    
    * Rename store
    
    * Get data in Redux Devtools

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 
 module.exports = {
   reactStrictMode: true,
+  env: {
+    API_KEY: process.env.REACT_APP_API_KEY,
+    DB_ID: process.env.REACT_APP_DB_ID
+  },
   distDir: 'build',
   eslint: {
     // Warning: This allows production builds to successfully complete even if
