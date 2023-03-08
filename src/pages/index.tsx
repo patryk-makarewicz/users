61b7d3612131526a3dc177f62ddbab8c40491176
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2023-03-09 00:56:48 +0100

    Add edit and update functionality

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,13 +4,21 @@ import { useForm, SubmitHandler } from 'react-hook-form';
 
 import { useAppDispatch, useAppSelector } from 'src/state/hooks';
 import { wrapper } from 'src/state/store';
-import { getUsersListRequest, deleteUserRequest, createUserRequest } from 'src/state/users/actions';
+import {
+  getUsersListRequest,
+  deleteUserRequest,
+  createUserRequest,
+  updateUserRequest
+} from 'src/state/users/actions';
 
 type Inputs = {
-  fullName: string;
-  userName: string;
-  email: string;
-  city: string;
+  id: string;
+  fields: {
+    fullName: string;
+    userName: string;
+    email: string;
+    city: string;
+  };
 };
 
 const Home = () => {
@@ -20,27 +28,50 @@ const Home = () => {
     register,
     handleSubmit,
     formState: { errors },
-    reset
+    reset,
+    setValue
   } = useForm<Inputs>();
+  // const onSubmit: SubmitHandler<Inputs> = (data) => {
+  //   dispatch(
+  //     createUserRequest({
+  //       records: [
+  //         {
+  //           fields: {
+  //             city: data.fields.city,
+  //             email: data.fields.email,
+  //             fullName: data.fields.fullName,
+  //             userName: data.fields.userName
+  //           }
+  //         }
+  //       ]
+  //     })
+  //   );
+  //   reset();
+  // };
+
   const onSubmit: SubmitHandler<Inputs> = (data) => {
-    dispatch(createUserRequest({ records: [{ fields: data }] }));
+    console.log(data);
+    dispatch(updateUserRequest({ records: [data] }));
     reset();
   };
 
   const dispatch = useAppDispatch();
-  const { data, statusGetUsersList, statusDeleteUser, statusCreateUser } = useAppSelector(
-    (state) => state.usersList
-  );
+  const { data, statusGetUsersList, statusDeleteUser, statusCreateUser, statusUpdateUser } =
+    useAppSelector((state) => state.usersList);
 
   const onDeleteUser = (idUser: string) => {
     dispatch(deleteUserRequest(idUser));
   };
 
   useEffect(() => {
-    if (statusDeleteUser === 'succeeded' || statusCreateUser === 'succeeded') {
+    if (
+      statusDeleteUser === 'succeeded' ||
+      statusCreateUser === 'succeeded' ||
+      statusUpdateUser === 'succeeded'
+    ) {
       dispatch(getUsersListRequest());
     }
-  }, [statusDeleteUser, statusCreateUser]);
+  }, [statusDeleteUser, statusCreateUser, statusUpdateUser]);
 
   // ----- TODO -----> handle failed status on actions
 
@@ -57,7 +88,17 @@ const Home = () => {
       <div>
         {data.map((user) => (
           <div key={user.id}>
-            {user.fields.fullName} <button>Edit</button>
+            {user.fields.fullName}{' '}
+            <button
+              onClick={() => {
+                setValue('fields.fullName', user.fields.fullName);
+                setValue('fields.userName', user.fields.userName);
+                setValue('fields.email', user.fields.email);
+                setValue('fields.city', user.fields.city);
+                setValue('id', user.id);
+              }}>
+              Edit
+            </button>
             <button onClick={() => onDeleteUser(user.id)}>Delete</button>
           </div>
         ))}
@@ -65,17 +106,19 @@ const Home = () => {
 
       <div>
         <form onSubmit={handleSubmit(onSubmit)}>
-          <input {...register('fullName', { required: true })} />
-          {errors.fullName && <span>This field is required</span>}
+          <input {...register('fields.fullName', { required: true })} />
+          {errors.fields?.fullName && <span>This field is required</span>}
 
-          <input {...register('userName', { required: true })} />
-          {errors.userName && <span>This field is required</span>}
+          <input {...register('fields.userName', { required: true })} />
+          {errors.fields?.userName && <span>This field is required</span>}
 
-          <input {...register('email', { required: true })} />
-          {errors.fullName && <span>This field is required</span>}
+          <input {...register('fields.email', { required: true })} />
+          {errors.fields?.fullName && <span>This field is required</span>}
 
-          <input {...register('city', { required: true })} />
-          {errors.userName && <span>This field is required</span>}
+          <input {...register('fields.city', { required: true })} />
+          {errors.fields?.city && <span>This field is required</span>}
+
+          <input {...register('id')} />
 
           <input type="submit" />
         </form>
