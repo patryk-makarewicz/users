a598c6cb30bbe8678a3a10270e04fe2d71d47a16
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2023-03-08 20:23:50 +0100

    Add next-redux-wrapper

--- i18n.js
+++ i18n.js
@@ -6,12 +6,12 @@ i18n
   .use(Backend)
   .use(initReactI18next)
   .init({
-    lng: 'pl',
+    lng: 'en',
     backend: {
       /* translation file path */
       loadPath: '/locales/{{lng}}/common.json'
     },
-    fallbackLng: 'pl',
+    fallbackLng: 'en',
     debug: false,
     /* can have multiple namespace, in case you want to divide a huge translation into smaller pieces and load them on demand */
     keySeparator: '.',
