891425d0dd723b9858652304c319a616453dd606
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2023-03-08 21:17:09 +0100

    Add deleteUserRequest action

--- src/state/users/actions.ts
+++ src/state/users/actions.ts
@@ -1,8 +1,14 @@
-import { getUsersList } from '@api/users/users.api';
+import { deleteUser, getUsersList } from '@api/users/users.api';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 export const getUsersListRequest = createAsyncThunk('users/getUsersList', async () => {
   const response = await getUsersList();
 
   return response.records;
+});
+
+export const deleteUserRequest = createAsyncThunk('users/deleteUser', async (userId: string) => {
+  const response = await deleteUser(userId);
+
+  return response;
 });
