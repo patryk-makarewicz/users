6ce04cdfd0363b3a4486bf9bdca16101f0c0a7c2
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2023-03-08 22:19:18 +0100

    Create basic form to add user

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,48 @@
 import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useForm, SubmitHandler } from 'react-hook-form';
+
 import { useAppDispatch, useAppSelector } from 'src/state/hooks';
 import { wrapper } from 'src/state/store';
-import { getUsersListRequest, deleteUserRequest } from 'src/state/users/actions';
+import { getUsersListRequest, deleteUserRequest, createUserRequest } from 'src/state/users/actions';
+
+type Inputs = {
+  fullName: string;
+  userName: string;
+  email: string;
+  city: string;
+};
 
 const Home = () => {
   const { t } = useTranslation();
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    dispatch(createUserRequest({ records: [{ fields: data }] }));
+    reset();
+  };
+
   const dispatch = useAppDispatch();
-  const { data, statusGetUsersList, statusDeleteUser } = useAppSelector((state) => state.usersList);
+  const { data, statusGetUsersList, statusDeleteUser, statusCreateUser } = useAppSelector(
+    (state) => state.usersList
+  );
 
   const onDeleteUser = (idUser: string) => {
     dispatch(deleteUserRequest(idUser));
   };
 
   useEffect(() => {
-    if (statusDeleteUser === 'succeeded') {
+    if (statusDeleteUser === 'succeeded' || statusCreateUser === 'succeeded') {
       dispatch(getUsersListRequest());
     }
-  }, [statusDeleteUser]);
+  }, [statusDeleteUser, statusCreateUser]);
+
+  // ----- TODO -----> handle failed status on actions
 
   return (
     <main>
@@ -28,7 +52,7 @@ const Home = () => {
       <h4>{statusGetUsersList === 'succeeded' && 'Success'}</h4>
 
       <button onClick={() => dispatch(getUsersListRequest())}>Refetch data</button>
-      <button>Add</button>
+      {/* <button onClick={() => dispatch(createUserRequest())}>Add</button> */}
 
       <div>
         {data.map((user) => (
@@ -37,6 +61,24 @@ const Home = () => {
             <button onClick={() => onDeleteUser(user.id)}>Delete</button>
           </div>
         ))}
+      </div>
+
+      <div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input {...register('fullName', { required: true })} />
+          {errors.fullName && <span>This field is required</span>}
+
+          <input {...register('userName', { required: true })} />
+          {errors.userName && <span>This field is required</span>}
+
+          <input {...register('email', { required: true })} />
+          {errors.fullName && <span>This field is required</span>}
+
+          <input {...register('city', { required: true })} />
+          {errors.userName && <span>This field is required</span>}
+
+          <input type="submit" />
+        </form>
       </div>
     </main>
   );
