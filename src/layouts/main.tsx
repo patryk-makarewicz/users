b5dd449a30c37dcb89a0c58d8df1f2051010c23d
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2023-03-10 01:55:37 +0100

    Update links to website

--- src/layouts/main.tsx
+++ src/layouts/main.tsx
@@ -13,8 +13,8 @@ const Layout = ({ children }: { children?: React.ReactNode }) => {
         <meta property="og:type" content="website" />
         <meta property="og:description" content="Users App - This is the website to manage users" />
         <meta property="og:title" content="Users App" />
-        <meta property="og:url" content="https://" />
-        <meta property="og:image" content="https:///cover.png" />
+        <meta property="og:url" content="https://makadev-users-manager.netlify.app/" />
+        <meta property="og:image" content="https://makadev-users-manager.netlify.app/cover.png" />
         <link
           href="https://fonts.googleapis.com/css2?family=Archivo:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
