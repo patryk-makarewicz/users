610050c8611333f420fa046902848dcd9e67b1d8
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2023-03-11 23:18:01 +0100

    Add isPageLoading on start

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,33 @@
+import { useEffect, useState } from 'react';
 import type { AppProps } from 'next/app';
-import '../../i18n';
-import MainLayout from '../layouts/main';
 import { wrapper } from 'src/state/store';
+import '../../i18n';
+
+import MainLayout from '../layouts/main';
+import { Spinner } from '@components/spinner';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [isPageLoading, setIsPageLoading] = useState(true);
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.onload = function () {
+        setIsPageLoading(false);
+      };
+    }
+  }, []);
+
   return (
-    <MainLayout>
-      <Component {...pageProps} />
-    </MainLayout>
+    <>
+      {isPageLoading ? (
+        <div style={{ height: '100vh' }}>
+          <Spinner width="50px" height="50px" />
+        </div>
+      ) : (
+        <MainLayout>
+          <Component {...pageProps} />
+        </MainLayout>
+      )}
+    </>
   );
 }
 
