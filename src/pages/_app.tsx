80425c09e698072b6b1e10215bb041e9f8dc1f62
Patryk Makarewicz <64835775+patryk-makarewicz@users.noreply.github.com>
2024-03-24 22:11:40 +0100

    Remove redundant files (#28)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,38 +1,15 @@
 import type { AppProps } from 'next/app';
 
 import { appWithTranslation } from 'next-i18next';
-import { useEffect, useState } from 'react';
 
-import { ImageOnLoad } from '@/helpers/imageOnLoad';
 import MainLayout from '@/layouts/main';
 import { wrapper } from '@/state/store';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [loadingPage, setLoadingPage] = useState(true);
-
-  useEffect(() => {
-    const handleLoad = () => {
-      setLoadingPage(false);
-    };
-
-    if (document.readyState === 'complete') {
-      handleLoad();
-    } else {
-      window.addEventListener('load', handleLoad);
-    }
-
-    return () => {
-      window.removeEventListener('load', handleLoad);
-    };
-  }, []);
-
   return (
-    <>
-      {loadingPage && <ImageOnLoad />}
-      <MainLayout loadingPage={loadingPage}>
-        <Component {...pageProps} />
-      </MainLayout>
-    </>
+    <MainLayout>
+      <Component {...pageProps} />
+    </MainLayout>
   );
 }
 
